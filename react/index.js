9ce491674db5007598d292869bc2f2c13d842d3f
DelttRaj <rroy4@deloitte.com>
2024-12-12 18:19:59 +0530

    pushing react changes

--- react/index.js
+++ react/index.js
@@ -1,10 +1,13 @@
 import { HeroBanner } from './heroBanner.js';
+import {Teaser} from "./teaser.js";
+
  
 export const App = () => { 
     return React.createElement(
         'div',
         null,
         React.createElement(HeroBanner),
+        React.createElement(Teaser)
     );
 };
 
