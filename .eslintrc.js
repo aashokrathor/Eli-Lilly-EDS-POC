982d4dca6881deb6a78329badce1a14e7297545c
DelttRaj <rroy4@deloitte.com>
2024-12-12 18:27:11 +0530

    removing the rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   root: true,
-  extends: 'airbnb-base',
   env: {
     browser: true,
   },
@@ -10,10 +9,6 @@ module.exports = {
     sourceType: 'module',
     requireConfigFile: false,
   },
-  rules: {
-    'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
-    'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
-    'no-param-reassign': [2, { props: false }], // allow modifying properties of param
-  },
+  rules: {},
   ignorePatterns: ['**/*.js'],
 };
